1d904f4bb2c54dbf4575a36dbe76be7dead51d8f
Aman Kumar <kumaraman6012@gmail.com>
2024-10-04 23:34:31 +0530

    modified

--- Server/server.js
+++ Server/server.js
@@ -28,45 +28,45 @@ mongoose
 
 app.use('/api', apiRoute)
 
-// const corsOptions = {
-//   origin: (origin, callback) => {
-//     if (!origin) {
-//       return callback(null, true);
-//     }
-//     if (process.env.NODE_ENV === 'development') {
-//       return callback(null, true);
-//     }
-//     const allowedOrigins = ['https://salonbookingtime.vercel.app'];
-//     if (allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   credentials: true,
-//   methods: ["GET,POST,PUT,DELETE,PATCH,HEAD"],
-//   allowedHeaders: 'Content-Type, Authorization',
-// };
-  const prodOrigins = [process.env.ORIGIN_1].filter(Boolean)
-  const devOrigins = ['http://localhost:5173']
-  const allowedOrigins = process.env.NODE_ENV === 'development' ? devOrigins : prodOrigins
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin) {
+      return callback(null, true);
+    }
+    if (process.env.NODE_ENV === 'development') {
+      return callback(null, true);
+    }
+    const allowedOrigins = ['https://salonbookingtime.vercel.app'];
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ["GET,POST,PUT,DELETE,PATCH,HEAD"],
+  allowedHeaders: 'Content-Type, Authorization',
+};
 
-  app.use(cors({
-    origin:(origin,callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        console.log(origin,allowedOrigins)
-        callback(null,true);
-      }else{
-        callback(new Error("Not allwed by CORS"));
-      }
-    },
-    credentials: true,
-    methods: ["GET,POST,PUT,DELETE,PATCH,HEAD"],
-    allowedHeaders: 'Content-Type, Authorization',
-  }))
+  // const prodOrigins = [process.env.ORIGIN_1].filter(Boolean)
+  // const devOrigins = ['http://localhost:5173']
+  // const allowedOrigins = process.env.NODE_ENV === 'development' ? devOrigins : prodOrigins
+  // app.use(cors({
+  //   origin:(origin,callback) => {
+  //     if (!origin || allowedOrigins.includes(origin)) {
+  //       console.log(origin,allowedOrigins)
+  //       callback(null,true);
+  //     }else{
+  //       callback(new Error("Not allwed by CORS"));
+  //     }
+  //   },
+  //   credentials: true,
+  //   methods: ["GET,POST,PUT,DELETE,PATCH,HEAD"],
+  //   allowedHeaders: 'Content-Type, Authorization',
+  // }))
 
 
-    // app.use(cors(corsOptions));
+    app.use(cors(corsOptions));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }))
     app.use(cookieParser())
