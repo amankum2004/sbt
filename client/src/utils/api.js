a31080a88b702cff8cc36bf846982f0574433e80
Aman Kumar <kumaraman6012@gmail.com>
2024-10-04 10:45:27 +0530

    api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const baseUrl =
   import.meta.env.VITE_environment === 'development'
     ? 'http://localhost:8000/api'
-    : 'https://spectacular-treacle-e92e0b.netlify.app/api'
+    : 'https://salonbookingtime.vercel.app'
 export const api = axios.create({
   baseURL: baseUrl,
   withCredentials: true
