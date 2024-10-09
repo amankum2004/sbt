b8478c8852100b33cf310d87414a54d3f00e9160
Aman Kumar <kumaraman6012@gmail.com>
2024-10-09 14:25:24 +0530

    modified css

--- Server/models/service-model.js
+++ Server/models/service-model.js
@@ -1,13 +1,13 @@
-const {Schema, model} = require("mongoose");
+// const {Schema, model} = require("mongoose");
 
 
-const serviceSchema = new Schema({
-    service:{type:String,required:true},
-    description:{type:String,required:true},
-    provider:{type:String,required:true},
-})
+// const serviceSchema = new Schema({
+//     service:{type:String,required:true},
+//     description:{type:String,required:true},
+//     provider:{type:String,required:true},
+// })
 
-const Service = new model("Service",serviceSchema);
+// const Service = new model("Service",serviceSchema);
 
 
-module.exports = Service;
+// module.exports = Service;
--- Server/models/timeSlot-model.js
+++ Server/models/timeSlot-model.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose")
+const cron = require("node-cron")
 
 const timeSchema = new mongoose.Schema({
   _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
   date: { type: Date, required: true },
-  is_booked: { type: Boolean, default: false,
-  },
+  is_booked: { type: Boolean, default: false},
 })
 
 const timeSlotSchema = new mongoose.Schema({
@@ -32,6 +32,24 @@ const timeSlotSchema = new mongoose.Schema({
   });
   
   const TimeSlot = mongoose.model('TimeSlot', timeSlotSchema);
+
+  // Scheduling job to run every day at midnight (00:00)
+  cron.schedule("0 0 * * *", async () => {
+  console.log("Running scheduled task to delete past time slots...");
+  try {
+    // Get current date
+    const currentDate = new Date();
+
+    // Delete time slots where the date is less than the current date
+    const result = await TimeSlot.deleteMany({
+      date: { $lt: currentDate },
+    });
+
+    console.log(`Deleted ${result.deletedCount} expired time slots.`);
+  } catch (error) {
+    console.error("Error deleting expired time slots:", error);
+  }
+});
   
   module.exports = TimeSlot;
 
