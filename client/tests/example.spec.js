a424d42552be40ae3260a119248e7909c82759fc
Aman Kumar <kumaraman6012@gmail.com>
2026-01-15 14:06:29 +0530

    updated ci-cd file

--- client/tests/example.spec.js
+++ client/tests/example.spec.js
@@ -1,19 +1,38 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// test('Check ShopOwner profile', async ({ page }) => {
+//   await page.goto('https://www.salonhub.co.in/');
+//   await page.getByRole('button', { name: 'Book Salon Appointment Now' }).click();
+//   await page.getByRole('button', { name: 'Phone' }).click();
+//   await page.getByRole('textbox', { name: '0123456789' }).click();
+//   await page.getByRole('textbox', { name: '0123456789' }).fill('8810269376');
+//   await page.getByRole('textbox', { name: '0123456789' }).press('Tab');
+//   await page.getByRole('textbox', { name: 'Password' }).fill('1234');
+//   await page.getByRole('textbox', { name: 'Password' }).press('Enter');
+//   await page.getByRole('button', { name: 'Dashboard' }).click();
+//   await page.getByRole('img', { name: 'User Avatar' }).first().click();
+//   await page.getByRole('link', { name: 'Profile' }).click();
+//   await page.getByRole('img', { name: 'User Avatar' }).first().click();
+//   await page.getByRole('link', { name: 'My bookings' }).click();
+//   await page.getByRole('button', { name: 'History (1)' }).click();
+//   await page.getByRole('img', { name: 'User Avatar' }).first().click();
+//   await page.getByRole('link', { name: 'Book appointment' }).click();
+// });
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+// test('has title', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/Playwright/);
+// });
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
+
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
