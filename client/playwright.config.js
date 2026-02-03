f193e08b2bd112248dee3d2ce998281a76776bd0
Aman Kumar <kumaraman6012@gmail.com>
2026-02-03 14:22:11 +0530

    playwright testing auth session added

--- client/playwright.config.js
+++ client/playwright.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { open: 'always' }], ['json', { outputFile: 'test-results/results.json' }]],
+  reporter: [['html', { outputFolder:'test-results', open: 'always' }], ['json', { outputFile: 'test-results/results.json' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
